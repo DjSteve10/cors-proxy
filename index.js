dac62f0a08aafee399bffb3f5d6075f223666a88
DjSteve10 <84409107+DjSteve10@users.noreply.github.com>
2025-06-22 17:55:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const request = require('request');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use(cors());
 
-app.get('/proxy', (req, res) => {
-  const targetUrl = req.query.url;
-  if (!targetUrl) {
-    return res.status(400).send('Missing url parameter');
-  }
-
-  req.pipe(request(targetUrl)).pipe(res);
+app.use('/:url(*)', (req, res, next) => {
+  const target = req.params.url;
+  if (!/^https?:\/\//i.test(target)) return res.status(400).send('Invalid URL');
+  return createProxyMiddleware({ target, changeOrigin: true, pathRewrite: { ['^/' + target]: '' } })(req, res, next);
 });
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`Proxy server running on port ${PORT}`);
+  console.log(`CORS Proxy running on port ${PORT}`);
 });
